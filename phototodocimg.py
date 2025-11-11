02c5fc18828e51905c677258cbe8b08addce80e0
Robert Seward <rseward@bluestone-consulting.com>
2025-11-11 11:12:27 -0500

    Improved code documentation.

--- phototodocimg.py
+++ phototodocimg.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
-########################################################################################################################################
-# Rob Seward's Document Scanner Project
-########################################################################################################################################
+"""
+Simple Document Page Scanner Project based on OpenCV
+
+The project assumes a photo of a single A4 page in a photo mostly orientated with the top of the page up. 
+
+It will find the page and extract it into an image of it's own.
+
+You could use this image for further processing such as OCR feeding it to an AI or whatever.
+
+Author: rseward@bluestone-consulting.com
+Date: 2025-11-11
+"""
 
 import cv2
 import numpy as np
@@ -13,6 +22,11 @@ A4_HEIGHT_TO_WIDTH_RATIO = (297 / 210)
 debug = False
 
 def estimate_dims(perimeter):
+    """Estimate the dimensions of the document from the perimeter.
+       Here the assumptions that the page is a A4 page is most important as it estimates the
+       dimensions of the page based on the perimeter and this assumption.
+    """
+
     # Estimate the dimensions of the document
     # p = 2*w + 2*h
     # h = p/2 - w
@@ -28,7 +42,7 @@ def estimate_dims(perimeter):
 
 def order_points(pts):
     """Re-order the page points to be in the order of nw, sw, se, ne. This ordering assumes
-       the page in the image is orientated up mostly."""
+       the page in the image is orientated with top up mostly in the photo."""
     # pts is a numpy array of 4 points
     # Initialize a list of coordinates that will be ordered
     # such that the first entry is the top-left,
@@ -146,7 +160,7 @@ def main(image, ab_debug):
     process_image(img)
 
 """
-Test points for debugging
+Test points for debugging: scanned-form.jpg
 
 nw = (38, 298)
 sw = (171, 1280)
@@ -163,64 +177,6 @@ cv2.waitKey(0)
 #cv2.destroyAllWindows()
 #sys.exit(1)
 """
-
-# grabcut was problematic for this application. Retaining the code for reference of the experiment
-#  but in the end it didn't work well. Also it seemed slow.
-
-try_grabcut = False
-
-if try_grabcut:
-    # Create a mask initialized with GC_PR_BGD (probable background)
-    mask = np.full(page.shape[:2], cv2.GC_PR_BGD, dtype=np.uint8)
-
-    # Create a background model and a foreground model
-    bgdModel = np.zeros((1,65), np.float64)
-    fgdModel = np.zeros((1,65), np.float64)
-
-    # For testing Define the rectangle with foreground pixels for grabcut
-    #rect = (155, 280, 615, 875)
-    #rect = (nw[0], ne[1], se[0] - sw[0], se[1] - ne[1])
-    rect = (0, 0, page.shape[0], page.shape[1])
-
-    output = img.copy()
-    cv2.rectangle(output, rect, (0, 255, 0), 2)
-    cv2.imshow('Image', output)
-    cv2.waitKey(0)
-
-    (x, y, w, h) = rect
-
-    mask[y:y+h, x:x+w] = cv2.GC_PR_FGD
-
-    # Use GrabCut to binarize the image
-    cv2.grabCut(page, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
-
-    mask2 = np.where((mask==cv2.GC_FGD) + (mask==cv2.GC_PR_FGD), 255, 0).astype('uint8')
-
-    # Copy the foreground
-    output = cv2.bitwise_and(page, page, mask=mask2)
-
-    cv2.imshow('Image after grabcut', output)
-    cv2.waitKey(0)
-
-if False:
-    # Follow on code for grabcut, that was abandoned pretty early on.
-    # Find contours
-    contours, _ = cv2.findContours(fgdModel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Sort contours by area
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-    # Get the largest contour
-    contour = contours[0]
-
-    # Get the bounding box
-    x, y, w, h = cv2.boundingRect(contour)
-
-    print(f"x: {x}, y: {y}, w: {w}, h: {h}")
-
-    # Crop the image
-    img = img[y:y+h, x:x+w]
-
 
 
 if __name__ == '__main__':
